92d1a0a1b90d2e317cb7b0d9c67d30183a377025
Shay Subramanian <shay.subramanian@Shays-MacBook-Air.local>
2026-02-10 15:00:12 -0700

    Update readme, requirements, and refactor - change working name to mtn-grid

--- scripts/pull_activities.py
+++ scripts/pull_activities.py
@@ -1,5 +1,5 @@
-from strava_lab.strava_api import fetch_activities
-from strava_lab.storage import save_raw_activities
+from mtn_grid.strava_api import fetch_activities
+from mtn_grid.storage import save_raw_activities
 
 if __name__ == '__main__':
     activities = fetch_activities()
